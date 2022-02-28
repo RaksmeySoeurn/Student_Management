8c22112296a4819f19274f81429b587ac41e6d20
GURUTECH <guru.tech.kh@gmail.com>
2022-02-28 14:48:34 +0700

    design Layout form login

--- StudentManagement/Forms/Frm_Login.Designer.cs
+++ StudentManagement/Forms/Frm_Login.Designer.cs
@@ -28,13 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel_title = new System.Windows.Forms.Panel();
+            this.panel_Content = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // panel_title
+            // 
+            this.panel_title.BackColor = System.Drawing.Color.DimGray;
+            this.panel_title.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_title.Location = new System.Drawing.Point(0, 0);
+            this.panel_title.Name = "panel_title";
+            this.panel_title.Size = new System.Drawing.Size(700, 35);
+            this.panel_title.TabIndex = 0;
+            // 
+            // panel_Content
+            // 
+            this.panel_Content.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_Content.Location = new System.Drawing.Point(0, 35);
+            this.panel_Content.Name = "panel_Content";
+            this.panel_Content.Size = new System.Drawing.Size(700, 465);
+            this.panel_Content.TabIndex = 1;
             // 
             // Frm_Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 33F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(528, 216);
+            this.BackColor = System.Drawing.Color.Ivory;
+            this.ClientSize = new System.Drawing.Size(700, 500);
+            this.Controls.Add(this.panel_Content);
+            this.Controls.Add(this.panel_title);
+            this.Font = new System.Drawing.Font("Khmer OS Siemreap", 10.8F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.Name = "Frm_Login";
             this.Text = "Frm_Login";
             this.ResumeLayout(false);
@@ -42,5 +67,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel_title;
+        private System.Windows.Forms.Panel panel_Content;
     }
 }
