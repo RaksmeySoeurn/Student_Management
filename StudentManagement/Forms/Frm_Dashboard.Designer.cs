89ea7dde2ccd3bd865f6163ffba459241dd31cf7
GURUTECH <guru.tech.kh@gmail.com>
2022-03-03 08:33:21 +0700

    complete dashboard

--- StudentManagement/Forms/Frm_Dashboard.Designer.cs
+++ StudentManagement/Forms/Frm_Dashboard.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Frm_Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(385, 161);
+            this.Name = "Frm_Dashboard";
             this.Text = "Frm_Dashboard";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
